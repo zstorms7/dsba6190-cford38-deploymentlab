43c86efa59cf7fe3c387566d17051ef08e7984ba
zstorms7 <510storms@gmail.com>
2025-12-02 18:23:22 -0500

    Add virtual network, subnet, SQL server, database, and private endpoint resources

--- main.tf
+++ main.tf
@@ -43,4 +43,86 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
+# ---------------------------------------------------
+# VIRTUAL NETWORK + SUBNET
+# ---------------------------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-cford38-dev-eastus-001"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-dsba6190-cford38-dev-eastus-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  # Required for private endpoints
+  enforce_private_link_endpoint_network_policies = true
+}
+
+# ---------------------------------------------------
+# SQL SERVER
+# ---------------------------------------------------
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = "sql-dsba6190-cford38-dev-001"
+  resource_group_name          = "rg-dsba6190-beta-eastus-001"
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "StrongP@ssw0rd123"
+}
+
+# ---------------------------------------------------
+# SQL DATABASE
+# ---------------------------------------------------
+resource "azurerm_mssql_database" "db" {
+  name        = "db-dsba6190-cford38-dev-001"
+  server_id   = azurerm_mssql_server.sql_server.id
+  sku_name    = "S0"
+  max_size_gb = 5
+}
+
+# ---------------------------------------------------
+# PRIVATE ENDPOINT + PRIVATE DNS ZONE
+# (This is what “puts the database inside the subnet”)
+# ---------------------------------------------------
+
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dnslink" {
+  name                  = "dnslink-dsba6190"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "sql_pe" {
+  name                = "pe-sql-dsba6190-cford38-dev-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet.id
+
+  private_service_connection {
+    name                           = "sql-pe-connection"
+    private_connection_resource_id = azurerm_mssql_server.sql_server.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_zone_group" "dnsgroup" {
+  name                 = "sql-dns-zone-group"
+  resource_group_name  = azurerm_resource_group.rg.name
+  private_endpoint_id  = azurerm_private_endpoint.sql_pe.id
+
+  private_dns_zone_configs {
+    name              = "sql-zone-config"
+    private_dns_zone_id = azurerm_private_dns_zone.dns.id
+  }
+}
