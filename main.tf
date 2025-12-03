1dcc1b715682935ec8091b54d59c907b1127ea37
zstorms7 <510storms@gmail.com>
2025-12-02 20:52:48 -0500

    new push

--- main.tf
+++ main.tf
@@ -58,9 +58,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  # Required for private endpoints
-  enforce_private_link_endpoint_network_policies = true
 }
 
 # ---------------------------------------------------
